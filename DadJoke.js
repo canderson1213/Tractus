857b3ac9bf4209cba8966dc1f215324cdcbdcbef
canderson1213 <canderson1213@gmail.com>
2020-03-26 23:51:25 -0600

    final product

--- DadJoke.js
+++ DadJoke.js
@@ -1,44 +1,41 @@
-var jokeIn = document.getElementById('joke');
-var numOfJokes = document.getElementById('numOfJokes');
-let count = 0;
+function generateContainers(event){
+  event.preventDefault();
+  let numOfJokes = document.getElementById('numOfJokes').value;
+  let container = document.querySelector(".megaContainer");
+  container.innerHTML = "";
+  for ( let i = 0; i < numOfJokes; i++) {
+    let outerDiv = document.createElement("div");
+    outerDiv.setAttribute("class", "container");
+    container.appendChild(outerDiv );
 
-function generateContainers(){
+    let jokeHere = document.createElement("div");
+    jokeHere.setAttribute("id", "joke" + i);
+    jokeHere.setAttribute("class", "joke");
+    outerDiv.appendChild(jokeHere);
 
-  var outerDiv = document.createElement("div");
-  outerDiv.setAttribute("class", "container");
-  document.body.appendChild( outerDiv );
 
-  count = count + 1;
-  console.log(jokeIn);
-  var jokeHere = document.createElement("div");
-  jokeHere.setAttribute("id", "joke" + count);
-  jokeHere.setAttribute("class", "joke");
-  outerDiv.appendChild(jokeHere);
+    let button = document.createElement("button");
+    button.setAttribute("onclick", "generateJoke('joke" + i + "')");
+    button.setAttribute("id", "jokeButton" + i);
+    button.setAttribute("class", "button");
+    outerDiv.appendChild(button);
 
-  var placeHolder = document.createTextNode("Joke here");
-  jokeHere.appendChild(placeHolder);
-
-  var button = document.createElement("button");
-  button.setAttribute("onclick", "generateJoke()");
-  button.setAttribute("id", "jokeButton" + count);
-  button.setAttribute("class", "button");
-  outerDiv.appendChild(button);
-
-  var genJoke = document.createTextNode("Generate Joke");
-  button.appendChild(genJoke);
-
+    let genJoke = document.createTextNode("Generate Joke");
+    button.appendChild(genJoke);
+    generateJoke("joke" + i);
+  }
 }
 
-async function generateJoke(){
+async function generateJoke(id) {
+  let jokeElement = document.getElementById(id);
+  jokeElement.innerHTML = "Loading Joke...";
 
   const jokeResponse = await fetch('https://icanhazdadjoke.com/', {
     headers: {
       'Accept': 'application/json'
     }
   });
-let joke = await jokeResponse.json();
+  let joke = await jokeResponse.json();
 
-
-
-jokeIn.innerHTML = joke.joke;
+  jokeElement.innerHTML = joke.joke;
 }
